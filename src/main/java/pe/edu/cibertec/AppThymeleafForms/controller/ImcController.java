bd28ed2a7a55a61d8b9e4d2855c17fca223b75fa
LUIS SALVATIERRA <luisangel.salvat@gmail.com>
2023-04-08 20:19:48 -0500

    algoritmo calculo IMC

--- src/main/java/pe/edu/cibertec/AppThymeleafForms/controller/ImcController.java
+++ src/main/java/pe/edu/cibertec/AppThymeleafForms/controller/ImcController.java
@@ -3,6 +3,8 @@ package pe.edu.cibertec.AppThymeleafForms.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import pe.edu.cibertec.AppThymeleafForms.model.ImcForm;
 
 @Controller
@@ -12,6 +14,39 @@ public class ImcController {
     public String formImc(Model model){
         model.addAttribute("imcForm", new ImcForm());
         model.addAttribute("visualizar", false);
+        return "formImc";
+    }
+
+    @PostMapping("/calculoimc")
+    public String formImc(@ModelAttribute("imcForm") ImcForm imcForm,
+                          Model model){
+        Double tallam = imcForm.getTalla() / 100;
+        Double valorImc = imcForm.getPeso() / (tallam * tallam);
+        String resultadoImc ="";
+        String colorMensaje = "alert-warning";
+        if(valorImc <= 18.5){
+            resultadoImc = "Por debajo del peso";
+            colorMensaje = "alert-dark";
+        } else  if(valorImc <= 25){
+            resultadoImc = "Con peso normal";
+            colorMensaje = "alert-primary";
+        } else if(valorImc <= 30){
+            resultadoImc = "Con sobrepeso";
+            colorMensaje = "alert-warning";
+        } else if(valorImc <= 35){
+            resultadoImc = "Obesidad leve";
+            colorMensaje = "alert-danger";
+        } else if(valorImc <= 39){
+            resultadoImc = "Obesidad media";
+            colorMensaje = "alert-danger";
+        }else {
+            resultadoImc = "Obesidad mórbida";
+            colorMensaje = "alert-danger";
+        }
+        model.addAttribute("visualizar", true);
+        model.addAttribute("resultado", "Su valor de IMC es: "+
+                String.format("%.2f", valorImc)+ ", usted se encuentra "+ resultadoImc);
+        model.addAttribute("colormensaje", colorMensaje);
         return "formImc";
     }
 }
