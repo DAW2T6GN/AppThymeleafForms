92372825bed956ed60e66d4182edb7cc96cd3369
LUIS SALVATIERRA <luisangel.salvat@gmail.com>
2023-04-08 19:39:39 -0500

    Initial commit

---
+++ src/test/java/pe/edu/cibertec/AppThymeleafForms/AppThymeleafFormsApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.AppThymeleafForms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppThymeleafFormsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
